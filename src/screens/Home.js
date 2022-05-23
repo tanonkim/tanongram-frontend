e733928ec42318cff4c089cdd5b47d0f5312d615
tanonkim <robbins4bos@gmail.com>
2022-05-23 16:15:25 +0900

    add : login

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,10 @@
-import { isLoggedInVar } from "../apollo";
+import { logUserOut } from "../apollo";
 
 function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <button onClick={() => isLoggedInVar(false)}>Log out now!</button>
+      <button onClick={() => logUserOut()}>Log out now!</button>
     </div>
   );
 }
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -7,21 +7,64 @@ import Input from "../components/auth/Input";
 import Seperator from "../components/auth/Seperator";
 import PageTitle from "../components/pageTitle";
 import routes from "../routes";
+import FormError from "../components/auth/FormError";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useForm } from "react-hook-form";
-import FormError from "../components/auth/FormError";
+import { gql, useMutation } from "@apollo/client";
+import { logUserIn } from "../apollo";
+
+const LOGIN_MUTATION = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      ok
+      token
+      error
+    }
+  }
+`;
 
 function Login() {
-  const { register, handleSubmit, errors, formState } = useForm({
+  const {
+    register,
+    handleSubmit,
+    errors,
+    formState,
+    getValues,
+    setError,
+    clearErrors,
+  } = useForm({
     mode: "onChange",
   });
+  const onCompleted = (data) => {
+    const {
+      login: { ok, error, token },
+    } = data;
+    if (!ok) {
+      return setError("result", {
+        message: error,
+      });
+    }
+    if (token) {
+      logUserIn(token);
+    }
+  };
+  const [login, { loading }] = useMutation(LOGIN_MUTATION, {
+    onCompleted,
+  });
   const onSubmitValid = (data) => {
-    console.log(data);
+    if (loading) {
+      return;
+    }
+    const { username, password } = getValues();
+    login({
+      variables: { username, password },
+    });
   };
-  const onSubmitInvalid = (data) => {
-    console.log(data);
+  const clearLoginError = () => {
+    clearErrors("result");
   };
+
   return (
     <AuthLayout>
       <PageTitle title="Login" />
@@ -29,7 +72,7 @@ function Login() {
         <div>
           <FontAwesomeIcon icon={faInstagram} size="4x" />
         </div>
-        <form onSubmit={handleSubmit(onSubmitValid, onSubmitInvalid)}>
+        <form onSubmit={handleSubmit(onSubmitValid)}>
           <Input
             ref={register({
               required: "Username is required",
@@ -38,24 +81,30 @@ function Login() {
                 message: "Username must be longer than 5",
               },
             })}
+            onChange={clearLoginError}
             name="username"
             type="text"
             placeholder="Username"
             hasError={Boolean(errors?.username?.message)}
           />
           <FormError message={errors?.username?.message}></FormError>
-
           <Input
             ref={register({
               required: "Password is required",
             })}
+            onChange={clearLoginError}
             name="password"
             type="password"
             placeholder="Password"
             hasError={Boolean(errors?.password?.message)}
           />
           <FormError message={errors?.password?.message}></FormError>
-          <Button type="submit" value="Log in" disabled={!formState.isValid} />
+          <Button
+            type="submit"
+            value={loading ? "Loading..." : "Log in"}
+            disabled={!formState.isValid || loading}
+          />
+          <FormError message={errors?.result?.message}></FormError>
         </form>
         <Seperator />
         <FacebookLogin ctx="Login With FaceBook"></FacebookLogin>

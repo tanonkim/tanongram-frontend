149dabdd66fabb798471bc7ea8913a3f950f8af0
tanonkim <robbins4bos@gmail.com>
2022-05-23 15:10:30 +0900

    add : Apollo setup

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import NotFound from "./screens/Notfound";
 import SignUp from "./screens/SignUp";
 import routes from "./routes";
 
-import { useReactiveVar } from "@apollo/client";
+import { ApolloProvider, useReactiveVar } from "@apollo/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { darkModeVar, isLoggedInVar } from "./apollo";
+import { client, darkModeVar, isLoggedInVar } from "./apollo";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, GlobalStyles, lightTheme } from "./styles";
 import { HelmetProvider } from "react-helmet-async";
@@ -15,23 +15,25 @@ function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
   return (
-    <HelmetProvider>
-      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-        <GlobalStyles />
-        <BrowserRouter>
-          <Routes>
-            <Route
-              path={routes.home}
-              element={isLoggedIn === true ? <Home /> : <Login />}
-            />
-            {!isLoggedIn ? (
-              <Route path={routes.signUp} element={<SignUp />}></Route>
-            ) : null}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-    </HelmetProvider>
+    <ApolloProvider client={client}>
+      <HelmetProvider>
+        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+          <GlobalStyles />
+          <BrowserRouter>
+            <Routes>
+              <Route
+                path={routes.home}
+                element={isLoggedIn === true ? <Home /> : <Login />}
+              />
+              {!isLoggedIn ? (
+                <Route path={routes.signUp} element={<SignUp />}></Route>
+              ) : null}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+      </HelmetProvider>
+    </ApolloProvider>
   );
 }
 

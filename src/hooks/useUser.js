e0035bd9c23f2f2b8b739fe2273d23dae09ce9ee
tanonkim <robbins4bos@gmail.com>
2022-05-24 14:48:37 +0900

    add : avatar

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -5,6 +5,7 @@ import { isLoggedInVar, logUserOut } from "../apollo";
 const ME_QUERY = gql`
   query me {
     me {
+      id
       username
       avatar
     }
@@ -21,7 +22,7 @@ function useUser() {
       logUserOut();
     }
   }, [data]);
-  return;
+  return { data };
 }
 
 export default useUser;

c5981e30871994653b7302e64576209f7c5a8594
tanonkim <robbins4bos@gmail.com>
2022-05-25 18:00:29 +0900

    add : read/writeFragment

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
-import styled from "styled-components";
-import Avatar from "../components/Avatar";
-import { FatText } from "../components/shared";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBookmark,
-  faComment,
-  faHeart,
-  faPaperPlane,
-} from "@fortawesome/free-regular-svg-icons";
+import Photo from "../components/feed/Photo";
+import PageTitle from "../components/pageTitle";
 
 const FEED_QUERY = gql`
   query seeFeed {
@@ -24,91 +16,20 @@ const FEED_QUERY = gql`
       comments
       createdAt
       isMine
+      isLiked
     }
   }
-`;
-
-const PhotoContainer = styled.div`
-  background-color: white;
-  border: 1px solid ${(props) => props.theme.borderColor};
-  max-width: 615px;
-  margin: 20px auto 20px auto;
-`;
-
-const PhotoHeader = styled.div`
-  padding: 15px;
-  display: flex;
-  align-items: center;
-`;
-
-const Username = styled(FatText)`
-  margin-left: 15px;
-`;
-
-const PhotoFile = styled.img`
-  width: 100%;
-`;
-
-const PhotoData = styled.div``;
-
-const PhotoActions = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 15px;
-  div {
-    display: flex;
-    align-items: center;
-  }
-`;
-
-const PhotoAction = styled.div`
-  margin-right: 10px;
-`;
-
-const Likes = styled.div`
-  margin-left: 25px;
-  margin-bottom: 20px;
-  display: block;
 `;
 
 function Home() {
   const { data } = useQuery(FEED_QUERY);
   return (
     <div>
+      <PageTitle title="Home" />
       {data?.seeFeed?.map((photo) => (
-        <PhotoContainer key={photo.id}>
-          <PhotoHeader>
-            <Avatar lg url={photo.user.avatar} />
-            <Username>{photo.user.username}</Username>
-          </PhotoHeader>
-          <PhotoFile src={photo.file} />
-          <PhotoData>
-            <PhotoActions>
-              <div>
-                <PhotoAction></PhotoAction>
-                <FontAwesomeIcon size={"2x"} icon={faHeart} />
-                <PhotoAction></PhotoAction>
-                <FontAwesomeIcon size={"2x"} icon={faComment} />
-                <PhotoAction></PhotoAction>
-                <FontAwesomeIcon size={"2x"} icon={faPaperPlane} />
-              </div>
-              <div>
-                <FontAwesomeIcon size={"2x"} icon={faBookmark} />
-              </div>
-            </PhotoActions>
-            <Likes>
-              {photo.likes === 1 || 0
-                ? `${photo.likes} like`
-                : `${photo.likes} likes`}{" "}
-            </Likes>
-          </PhotoData>
-        </PhotoContainer>
+        <Photo key={photo.id} {...photo} />
       ))}
     </div>
   );
 }
 export default Home;
-//const navigate = useNavigate();
-//<h1>Home</h1>
-//<button onClick={() => logUserOut(navigate)}>Log out now!</button>

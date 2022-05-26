86207a3dd2562d029805b8531094b88b0acfd274
tanonkim <robbins4bos@gmail.com>
2022-05-26 15:24:35 +0900

    add : fragments

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,7 +1,34 @@
+import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router";
+import { PHOTO_FRAGMENT } from "../fragments";
+
+const SEE_PROFILE_QUERY = gql`
+  query seeProfile($username: String!) {
+    seeProfile(username: $username) {
+      firstName
+      lastName
+      username
+      bio
+      avatar
+      photos {
+        ...PhotoFragement
+      }
+      totalFollowing
+      totalFollowers
+      isMe
+      isFollowing
+    }
+  }
+  ${PHOTO_FRAGMENT}
+`;
 
 function Profile() {
   const { username } = useParams();
+  const { data } = useQuery(SEE_PROFILE_QUERY, {
+    variables: {
+      username,
+    },
+  });
   return "Profile";
 }
 
